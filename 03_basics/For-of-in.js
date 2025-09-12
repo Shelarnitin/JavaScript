f67e72659eb5293f5ad78b00efe66fe37d677569
Shelarnitin <shelarnitin399@gmail.com>
2025-09-12 21:41:31 +0530

    For-Of Loop

---
+++ 03_basics/For-of-in.js
@@ -0,0 +1,51 @@
+/********For-of loop***************/
+//used to iterate over iterable objects like arrays, strings, maps, sets.
+
+// Loop over array of fruits
+
+// let fruits = ["Mango", "banana", "apple"];
+// for(let fruit of fruits){
+//     console.log(fruit)
+// }
+
+
+// Loop over character in a string
+
+// let word = "Neo";
+// for(let char of word){
+//     console.log(char)
+// }
+
+
+// calculate sum of numbers in array
+
+// let nums = [5,10,15];
+// let total = 0;
+// for(let n of nums){
+//     total = total + n;
+// }
+// console.log("Total:",total)
+
+
+// Iterate over a set(Unique values)
+
+// let Unique = new Set([1,2,2,3]);   // set removes duplicate values
+// for(let val of Unique){
+//     console.log(val)
+// }
+
+
+// Iterate over map (Key-value pairs)
+
+// let user = new Map([
+//     ["name","Nitin"],
+//     ["city","pune"],
+// ]);
+// for(let[key,value]of user){
+//     console.log(`${key}; ${value}`)
+// }
+
+
+
+
+/*******************For-In Loop****************************/
