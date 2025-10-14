e3d1bd95a394d934bc3a2772f3f5ca731032a712
Shelarnitin <shelarnitin399@gmail.com>
2025-10-14 11:02:08 +0530

    practice on function

--- 02_basics/functions.js
+++ 02_basics/functions.js
@@ -1,68 +1,70 @@
 // basic syntax
-// function greet(){
-//     console.log("hello");
-// }
-// greet();
-
-// function add(a,b){
-//     return a+b;
-// }
-// console.log(add(2,3))
+function greet(){
+    console.log("hello");
+}
+greet();
+/****************************/
+function add(a,b){
+    return a+b;
+}
+console.log(add(2,3))
 
 // (1) Function declaration(named function)
 // Define with function keyword
 
-// function sayHi() {
-//     return "Hi";
-// }
-// console.log(sayHi());
+function sayHi() {
+    return "Hii"
+}
+console.log(sayHi())
 
 //NOTE: Hoisted (can be called before declaration)
 
 /***(2) function Expression * stored inside a variable***/
 
-// let greet = function(name) {
-//     return "Hello " + name;
-// };
-// console.log(greet("Nitin"));
+let hello = function(name) {
+    return "Hello " + name
+}
+console.log(hello("Nitin"))
+
 // NOTE: Note Hoisted
 
 
 /*******(3) Arrow functions (ES6) * shorter syntax, this behaves differently*********/
-// let squareTwo = (n,m) => {
-//    return [n*n, m*m];
-// } 
-    
-// console.log(squareTwo(5,6)); // for multiple number
+let squareTwo = (a,b) => {
+    return [ a*a, b*b]
+}
+console.log(squareTwo(3,4)) // for multiple number
 
-//  let square = (a) => {
-//     return a*a;
-//  }
-//  console.log(square(9));  // for single digit
+ let square = (a) => {
+    return a*a
+ } 
+ console.log(square(8)) // for single digit
 
 
 /********(4) Anonymous function * function without a name, usually used in inline********/
-// setTimeout(function() {
-//     console.log("I`am Anonymous")
-// }, 2000);
-// console.log("Wait.....")
+// setTimeout ( function() {
+//     console.log('i am anonymous function')
+// },2000)
+// console.log("...wait")
 
 /*********(5) * Immediatly invoked Function exprission(IIFE)************/
 // runs instantly after creation
 // (function () {
-//     console.log("IIFE execution");
+//     const message = "Hello from IIFE!";
+//     console.log(message);
 // })();
+// console.log(typeof message);
 
 
 /**********(6) * constructor Function*************/
 // used to create multiple similar objects
-// function person(name){
-//     this.name = name;
-// }
-// let p1 = new person("Nitin");
-// console.log(p1)
-// console.log(p1.name);
-// console.log("Hello ",p1.name)
+function person(name){
+    this.name = name;
+}
+let p1 = new person("Nitin");
+console.log(p1)
+console.log(p1.name);
+console.log("Hello ",p1.name)
 
 /***********(7) * Generator function****************************/
 // special function that can pause & resume with yield
@@ -81,18 +83,18 @@
 
 /************(8) * Async Function (ES8)*************************/
 // Handles promises with await
-// async function fetchData() {
-//     return "Data Received!";
-// }
-// fetchData().then(console.log) 
-// console.log(fetchData().then(console.log))  // promise { <pending> }
+async function fetchData() {
+    return "Data Received!";
+}
+fetchData().then(console.log) 
+console.log(fetchData().then(console.log))  // promise { <pending> }
 
 /********(9) * callback function************/
 // a function passed as an argument to another function
 
-// function processUserInput(callback){
-//     let name = "Nitin";
-//     callback(name);
-// }
-// processUserInput((n) => console.log("Hello " + n));
+function processUserInput(callback){
+    let name = "Nitin";
+    callback(name);
+}
+processUserInput((n) => console.log("Hello " + n));
 
