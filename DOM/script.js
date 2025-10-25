cf3cd7a2a9563ab5df799697e13dea71fcbe90d3
Shelarnitin <shelarnitin399@gmail.com>
2025-10-25 18:14:45 +0530

    about DOM

---
+++ DOM/script.js
@@ -0,0 +1,7 @@
+// console.log(document.getElementById('first'))
+const title = document.getElementById('first').innerHTML = "<h1>one two</h1>"
+title.style.backgroundcolor = "red"
+
+
+document.getElementsbyId('firstt').innerHTML = "<p>abcdefghijklmnopqrstuvwxyz</p>"
+console.log(title)
