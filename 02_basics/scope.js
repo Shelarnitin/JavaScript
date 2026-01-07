4fbcc4c8330aaf2a83fb4d64b1d2ed98c1d2725e
Shelarnitin <shelarnitin399@gmail.com>
2026-01-07 22:10:19 +0530

    Block Scope

--- 02_basics/scope.js
+++ 02_basics/scope.js
@@ -27,12 +27,12 @@ sayHi()
 console.log(msg);  // Error
 
 //*****BLOCK SCOPE*****/
-// if (true) {
-//     let x = 100;
-//     const y = 200;
-//     console.log(x,y);
+if (true) {
+    let x = 100;
+    const y = 200;
+    console.log(x,y);
     
-// }
+}
 // NOTE : var is not a block-scope it ignored {}
 
 // *****LEXICAL SCOPE*****
