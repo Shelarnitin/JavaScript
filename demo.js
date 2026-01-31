89a2be804833fa948075f638b977975e93ff4dba
Shelarnitin <shelarnitin399@gmail.com>
2026-01-31 13:34:10 +0530

    Math.floor

--- demo.js
+++ demo.js
@@ -3,8 +3,8 @@
 
 // console.log(Math.trunc(-3.7));
 
-console.log(Math.floor(4.9));
-console.log(Math.floor(4.1));
+// console.log(Math.floor(4.9));
+// console.log(Math.floor(4.1));
 
 console.log(Math.ceil(4.1));
 console.log(Math.ceil(-4.9));
