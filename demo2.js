b819078e804e632bdf0e1ca55e4a6e9fa15a80af
Shelarnitin <shelarnitin399@gmail.com>
2026-02-04 22:26:29 +0530

    Timeout function

--- demo2.js
+++ demo2.js
@@ -3,9 +3,9 @@
 let square = (n) => n*n;
 console.log(square(7))
 
-setTimeout(function() {
-    console.log("I Am Anonymous");
-},3000)
+// setTimeout(function() {
+//     console.log("I Am Anonymous");
+// },3000)
 
 function processUserInput(callback){
     let name = "Nitin";
