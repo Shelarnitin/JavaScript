f84906486398a1a9b68e2bef3e202b960902d183
Shelarnitin <shelarnitin399@gmail.com>
2026-02-09 14:19:00 +0530

    Global Scope

---
+++ ScopeDemo.js
@@ -0,0 +1,17 @@
+// let a = 10;
+// function test(){
+//     let b = 20;
+//     console.log(a)
+//     console.log(b)
+// }
+// test()
+// console.log(a)
+// console.log(b)
+
+// GLOBAL SCOPE => Accescible Everywhere
+ let str = "Neo";
+ function greet() {
+    console.log("Hello " + str)
+ }
+ greet()
+ console.log("Welcome " + str)
