b0971ba9c0c2fbc55ba56929e8ebe8c9e6f77acc
Shelarnitin <shelarnitin399@gmail.com>
2026-02-10 12:42:49 +0530

    Arrow function

--- FunctionDemo.js
+++ FunctionDemo.js
@@ -6,3 +6,23 @@ let obj = {
     }
 };
 obj.SayHii()
+
+// Arrow function and This
+let obj1 = {
+    namee : "Nitin",
+    SayHello: () => {
+        console.log("Hello " + this.namee)
+    }
+}
+obj1.SayHello() // Hello undefined
+
+// Arrow function inside method
+
+let obj2 = {
+    name: "Neo",
+    say: function (){
+        let arrow = () => console.log("Hii " + this.name)
+        arrow();
+    }
+}
+obj2.say()
