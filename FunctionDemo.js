0294a5ab086f232d1012931d545f896f4ec5504f
Shelarnitin <shelarnitin399@gmail.com>
2026-02-23 21:02:06 +0530

    Normal Function

--- FunctionDemo.js
+++ FunctionDemo.js
@@ -1,11 +1,11 @@
 // Normal function and this
-let obj = {
-    name : "Neo",
-    SayHii: function(){
-        console.log("Hello " + this.name);
-    }
-};
-obj.SayHii()
+// let obj = {
+//     name : "Neo",
+//     SayHii: function(){
+//         console.log("Hello " + this.name);
+//     }
+// };
+// obj.SayHii()
 
 // Arrow function and This
 let obj1 = {
