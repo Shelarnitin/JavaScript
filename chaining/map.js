26d86e5bb12f76bbc4447cea97608346b6f95cb9
Shelarnitin <shelarnitin399@gmail.com>
2026-02-25 20:25:53 +0530

    map

--- chaining/map.js
+++ chaining/map.js
@@ -3,9 +3,9 @@
 
 // Double each number
 
-let nums = [1,2,3,4];
-let doubled = nums.map(n => n*2);
-console.log(doubled)
+// let nums = [1,2,3,4];
+// let doubled = nums.map(n => n*2);
+// console.log(doubled)
 
 // Extract usernames
 
