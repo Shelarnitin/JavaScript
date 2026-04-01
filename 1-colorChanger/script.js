519836a1170cae05c0f0602185a045fd656597eb
Shelarnitin <shelarnitin399@gmail.com>
2026-04-01 21:11:45 +0530

    JS

--- 1-colorChanger/script.js
+++ 1-colorChanger/script.js
@@ -1,21 +1,9 @@
 const buttons = document.querySelectorAll('.button')
 const body = document.querySelector('body')
 
-
-buttons.forEach(function(button) {
-    console.log(button)
-    button.addEventListener("click", function(e){
-        console.log(e)
-        console.log(e.target)
-        if (e.target.id === "grey"){
-            body.style.backgroundColor = e.target.id
-        } else if (e.target.id === "blue"){
-            body.style.backgroundColor = e.target.id
-        }
-         else if (e.target.id === "white"){
-            body.style.backgroundColor = e.target.id
-        }
-         else if(e.target.id === "yellow"){
+buttons.forEach(function (button){
+    button.addEventListener('click', function(e){
+        if(e.target.id === e.target.id){
             body.style.backgroundColor = e.target.id
         }
     })
